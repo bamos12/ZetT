66896cdfd425bf991db234d0f9615801b5d6cd5f
bamos12 <touradama212@gmail.com>
2023-08-13 05:08:10 +0000

    lui et moi

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>amusons nous</title>
+</head>
+<body>
+
+	<h1>essai</h1>
+	<p>premier code</p>
+	<hr>
+	<form>
+		<p>Entrez un nombre entre 1 et 3</p>
+		<input type="texte" name="n">
+		<input type="submit" name="ok">
+
+
+
+	</form>
+
+
+</body>
+</html>
